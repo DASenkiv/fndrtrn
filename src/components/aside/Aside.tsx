ed81d3b6c2679247b18faa9b36fd3c9afa8e30ad
DASenkiv <dasenkiv@gmail.com>
2022-10-29 14:16:13 +0300

    feat: added simple structure of aside menu

--- src/components/aside/Aside.tsx
+++ src/components/aside/Aside.tsx
@@ -1,5 +1,12 @@
 import {FC} from "react";
 
+import './styles.css'
+
 export const Aside: FC = () => {
-    return (<aside>Aside</aside>)
+    return (<aside className={'aside'}> <a href={''}>Мой профиль</a>
+        <a href={''}>Я потерял</a>
+        <a href={''}>Я нашел</a>
+        <a href={''}>Сообщения</a>
+        <hr/>
+        <a href={''}>Написать в поддержку</a></aside>)
 }
